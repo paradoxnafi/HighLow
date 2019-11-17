1443d11094dde668e373b98f596e965cff7e02a5
paradoxnafi <paradoxnafi@protonmail.com>
2019-11-17 15:18:50 +0530

    First commit

---
+++ app/src/main/java/com/paradoxnafi/highlow/MainActivity.java
@@ -0,0 +1,45 @@
+package com.paradoxnafi.highlow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+	int randNum;
+
+	public String HighLow(int n, int r) {
+
+		if(n > r) {
+			return "Too high";
+		} else if(n < r){
+			return "Too low";
+		} else {
+			return "Correct!!!";
+		}
+
+	}
+
+	public void onClickCheck(View view) {
+
+		EditText editTextInputNum = (EditText) findViewById(R.id.editTextInputNum);
+		int number = Integer.parseInt(editTextInputNum.getText().toString());
+
+		String displayToast = HighLow(number, randNum);
+		Toast.makeText(MainActivity.this, displayToast, Toast.LENGTH_SHORT).show();
+	}
+
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		randNum = (int) (Math.random() * 50 + 1);
+	}
+}
